dec29b91c9f3742509e0d7266bb35148f2c6ce0a
Warren Uhrich <hello@warren.codes>
2020-07-22 09:56:57 -0600

    Add JS object and JSON example(s).

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,37 @@
-alert( "The script is loaded!" );
+// Store one value (variable as a string.)
+const myString = "Hello, World!";
+console.log( myString );
+
+// Store multiple values (multiple strings in an array variable.)
+const myArray = [myString, "A second string"];
+console.log( myArray );
+
+// Array of person data (name, age, hobbies.)
+const myPersonArray = [
+  "Henry", // Name
+  32, // Age
+  ["Program", "Hike", "Watch Movies"] // Hobbies
+];
+console.log( myPersonArray[2] ); // Display hobbies.
+
+/**
+ * JavaScript Objects
+ * 
+ * JSON - JavaScript Object Notation
+ */
+
+const myPersonObject = { // JavaScript object (not necessarily 100% valid JSON.)
+  "name":    "Henry",
+  "age":     32,
+  "hobbies": ["Program", "Hike", "Watch Movies"]
+};
+console.log( "My name is: " + myPersonObject.name );
+console.log( "I am " + myPersonObject.age + " years old." );
+console.log( "I like to " + myPersonObject.hobbies );
+
+// Output JavaScript object...
+console.log( myPersonObject );
+const validJSONString = JSON.stringify( myPersonObject );
+// Output perfectly valid JSON string...
+console.log( validJSONString ); // This can be interpreted by other programming languages / programs!
+// JSON helps us communicate in a universal format/standard.
